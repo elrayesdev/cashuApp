2973c51f2adca5f74492c9f1a09553797bc5379d
Mohammed EL Rayes <elrayesdev@gmail.com>
2021-06-27 11:15:57 +0200

    migrations creating tables

--- database/migrations/2021_06_27_090416_create_user_table.php
+++ database/migrations/2021_06_27_090416_create_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,25 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
+            //  table definition
+            $table->engine = 'innodb';
+            $table->collation = 'utf8mb4_bin';
+
+            // structure
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
+
+            // foreign keys
+            $table->unsignedBigInteger('customer_id');
+
+            // constrains
+            $table->foreign('customer_id')->references('id')->on('customer');
         });
     }
 
@@ -31,6 +42,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user');
     }
 }
